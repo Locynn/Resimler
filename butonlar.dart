aef8a8452946a7c7fc888bb829dcdb735b9e3f51
Locyn <96714850+Locynn@users.noreply.github.com>
2022-03-28 20:20:07 +0300

    butonlar.dart

---
+++ butonlar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Butonlar extends StatelessWidget {
+  const Butonlar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Buton Ekranı"),
+      ),
+      body: Center(),
+    );
+  }
+}
